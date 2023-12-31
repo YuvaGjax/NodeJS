2b4df34aedf4a0ce81ce035a8ffea73ee16d1171
Yuva <yuvaprasathg@gmail.com>
2023-12-31 12:18:58 +0530

    Code fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const app = require("./Server/app");
 
-const PORT = process.env.SERVER_PORT;
+const PORT = process.env.PORT || 4000;
 
 const server = http.createServer(app);
 
